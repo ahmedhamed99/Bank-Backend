5e680a7d60696518f1d486be00f42e5a05b2ae5d
ahmedhamed99 <ahmedhamed4121999@gmail.com>
2022-10-11 01:32:43 +0200

    first commit

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,20 +1,5 @@
 import {Pool} from "pg";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const {
-    POSTGRES_HOST,
-  POSTGRES_DB,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD
-} = process.env;
-
-const Client = new Pool({
-    host:POSTGRES_HOST,
-    database:POSTGRES_DB,
-    user:POSTGRES_USER,
-    password:POSTGRES_PASSWORD
-});
+const Client = new Pool();
 
 export default Client;
